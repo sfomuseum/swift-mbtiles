2584783970cd51743927eead9b317cf456e235df
thisisaaronland <devnull@localhost>
2020-11-17 11:41:47 -0500

    swift is weird

--- Sources/MBTiles/MBTile.swift
+++ Sources/MBTiles/MBTile.swift
@@ -3,4 +3,11 @@ public struct MBTile {
     public var z: String
     public var x: String
     public var y: String
+    
+    public init(prefix:String, z: String, x: String, y: String) {        
+        self.prefix = prefix
+        self.z = z
+        self.x = x
+        self.y = y
+    }
 }
