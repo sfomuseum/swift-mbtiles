b86d63c5458be720263ce75565dab4f8a15daaf6
thisisaaronland <devnull@localhost>
2020-11-18 17:57:27 -0500

    add SQLite.swift to SPM

--- Package.swift
+++ Package.swift
@@ -12,7 +12,8 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/ccgus/fmdb", .upToNextMinor(from: "2.7.7")),
-        .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0")
+        .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
+        .package(url: "https://github.com/stephencelis/SQLite.swift.git", from: "0.12.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -21,7 +22,8 @@ let package = Package(
             name: "MBTiles",
             dependencies: [
                 "FMDB",
-                "Logging"
+                "Logging",
+                "SQLite"
 	    ]),
         //.testTarget(
         //    name: "MBTilesTests",
