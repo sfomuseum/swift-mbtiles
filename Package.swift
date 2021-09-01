2da109ea3f9bd9bc96104ec492f49eee884e267e
thisisaaronland <aaron@localhost>
2021-09-01 13:07:41 -0700

    bump sqlite.swift version to 0.13.0

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/apple/swift-log.git", from: "1.4.2"),
-        .package(url: "https://github.com/stephencelis/SQLite.swift.git", from: "0.12.0")
+        .package(url: "https://github.com/stephencelis/SQLite.swift.git", from: "0.13.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
